ac3ec8dbd42c20efefda41d341b7dbc2938fe310
LeChustin <jostinaldaz4@gmail.com>
2024-02-14 09:54:02 -0500

    base de datos creada

---
+++ app/src/main/java/com/chstn/fichapersonal/DatabaseHelper.java
@@ -0,0 +1,56 @@
+package com.chstn.fichapersonal;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+    private static final String DATABASE_NAME = "Personas.db";
+    private static final String TABLE_NAME = "persona_table";
+    private static final String COL_1 = "ID";
+    private static final String COL_2 = "NOMBRE";
+    private static final String COL_3 = "EDAD";
+
+    private static final String COL_4 = "TELEFONO";
+
+    private static final String COL_5 = "DIRECCION";
+    private static final String COL_6 = "CORREO";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NOMBRE TEXT, EDAD INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String nombre, String edad, String telefono, String direccion, String correo) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, nombre);
+        contentValues.put(COL_3, edad);
+        contentValues.put(COL_4, telefono);
+        contentValues.put(COL_5, direccion);
+        contentValues.put(COL_6, correo);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        return result != -1;
+    }
+
+    // Método para recuperar los datos
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
+}
